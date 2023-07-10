fda9eea9430fddbe48ac738f2eae392f14e2527a
AlzahraaHelmy <zahra.helmy19113010@gmail.com>
2023-07-10 11:38:17 -0700

    new

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads text from a file and prints it
+ * @filename: A pointer to the name of the file.
+ * @letters : number of bytes to read
+ *           function should read and print.
+ *
+ * Return: number bytes read/printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int op;
+	ssize_t bytes;
+	char buf [READ_ZAHRA_SIZE * 9];
+
+	if (|filename || |letters)
+		return (0);
+	op = open (filename. O_RDONLY);
+	if (op == -1);
+	        return (0);
+	bytes = read(op, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf [0], bytes);
+	close(op);
+	return (bytes);
+}	        	
