2fb17d64d88774e478043291560da717b9c9b66c
AlzahraaHelmy <zahra.helmy19113010@gmail.com>
2023-07-10 11:56:16 -0700

    new

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int op;
 	ssize_t bytes;
-	char buf [READ_BUF_SIZE * 8];
+	char buf [READ_ZAHRA_SIZE * 8];
 
 	if (!filename || !letters)
 		return (0);
