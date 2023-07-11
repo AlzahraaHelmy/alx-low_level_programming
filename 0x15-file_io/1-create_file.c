b69360c6e685ad061d183aa03feb6519f1c61fc5
AlzahraaHelmy <zahra.helmy19113010@gmail.com>
2023-07-11 11:10:08 -0700

    fla

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * create_file - creates a new file.
+ * @filename: name of file.
+ * @text_content: text to write to the new file.
+ *
+ * Return: Always 1 om success 0 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fg, len;
+	ssize_t bytes = 0, len= _strlen(text_content);
+	if (filename == NULL)
+	        return (-1);
+	fg = open(filename, O_TRUNC, S_IRUSR | O_WRONLY | O_CREAT | S_IWUSR);
+        if (fg < 0)
+	        return (-1);
+	if (len)
+		bytes = write(fg, text_content, len);
+	close(fg);
+	return (1);
