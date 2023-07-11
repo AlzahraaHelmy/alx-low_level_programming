1452f43477a40c7194f14994356de72dcc9eff16
AlzahraaHelmy <zahra.helmy19113010@gmail.com>
2023-07-11 11:55:40 -0700

    fla

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,19 +13,15 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
     	        return (-1);
-        if (text_content != NULL)
 	{
 	while (text_content && text_content[len])
 		len++;
         }    
-
+        if (fg == -1 || len == -1)
+                return (-1);
         fg = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	bytes = write(fg, text_content, len);
 	
-        if (fg == -1 || len == -1)
-	        return (-1);
-	
 	close(fg);
-
 	return (1);
 }
