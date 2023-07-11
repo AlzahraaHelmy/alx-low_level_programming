1655f93293dfebcadc8bd94b167e703d72cc5da9
AlzahraaHelmy <zahra.helmy19113010@gmail.com>
2023-07-11 07:11:14 -0700

    za

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fg = open(filename, O_RDONLY);
-	if (fg == NULL)
+	if (fg == -1)
 		return (0);
         bytes = read(fg, &buf[0], letters);  
         bytes = write(STDOUT_FILENO, &buf[0], bytes);
