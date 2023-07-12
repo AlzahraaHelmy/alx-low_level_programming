68bbebac10a8301857cbc4f5bdf84aa928d3a1de
AlzahraaHelmy <zahra.helmy19113010@gmail.com>
2023-07-12 05:15:48 -0700

    cc

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,21 +25,17 @@ int _strlen(char *l)
  */
 int create_file(const char *filename, char *text_content);
 {
-	int fg, len, za;
+	int fg;
+        ssize_t bytes = 0, len = _strlen(text_content);
 
 	if (filename == NULL)
     	        return (-1);
 
         fg = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
         if (fg < 0)
-		return (-1);
-
-    	for (text_content && text_content[len])
-                len++;
-	
-	za  = write(fg, text_content, len);
-	if (za < 0)
-		return (-1)
+		return (-1);	
+        if (len)
+	        bytes = write(fg, text_content, len);
 	close(fg);
 	return (1);
 }
