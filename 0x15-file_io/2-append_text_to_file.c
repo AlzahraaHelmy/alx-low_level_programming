f8709dc7e6b93112768f3c46b12f3188d842dc2f
AlzahraaHelmy <zahra.helmy19113010@gmail.com>
2023-07-12 14:11:22 -0700

    fla

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,25 +9,25 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, a, b = 0;
+	int fld, wr, len = 0;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd < 0)
+	fld = open(filename, O_WRONLY | O_APPEND);
+	if (fld < 0)
 		return (-1);
 
 	if (text_content)
 	{
-		while (text_content[b])
-			b++;
-		a = write(fd, text_content, b);
-		if (a != b)
+		while (text_content[len])
+			len++;
+		wr = write(fld, text_content, b);
+		if (wr != len)
 			return (-1);
 	}
 
-	close(fd);
+	close(fld);
 
 	return (1);
 }
