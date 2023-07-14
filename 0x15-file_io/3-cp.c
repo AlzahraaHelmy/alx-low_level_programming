1bbb0ff5f8da0b5830175880869fc358031117e5
AlzahraaHelmy <zahra.helmy19113010@gmail.com>
2023-07-14 09:15:27 -0700

    za

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,16 +8,16 @@
  * @argv: arguments vector.
  * Return: no return.
  */
-void error_file(int file_from, int file_to, char *argv[])
+void error_file(int file_from, int file_to, char *av[])
 {
 	if (file_from < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 }
@@ -30,7 +30,7 @@ void error_file(int file_from, int file_to, char *argv[])
  */
 int main(int ac, char *av[])
 {
-	int file_from, file_to, err_close;
+	int file_from, file_to, erro_close;
 	ssize_t nchars, nu;
 	char buf[1024];
 
@@ -55,15 +55,15 @@ int main(int ac, char *av[])
 			error_file(0, -1, argv);
 	}
 
-	err_close = close(file_from);
-	if (err_close == -1)
+	erro_close = close(file_from);
+	if (erro_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 
-	err_close = close(file_to);
-	if (err_close == -1)
+	erro_close = close(file_to);
+	if (erro_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
