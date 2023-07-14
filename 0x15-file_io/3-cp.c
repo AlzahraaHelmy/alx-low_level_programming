2cb785fffdee54a323f27d0e21f4611a1adaa284
AlzahraaHelmy <zahra.helmy19113010@gmail.com>
2023-07-14 09:11:11 -0700

    za

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 
 /**
- * error_file - checks if files can be opened.
- * @file_from: file_from.
- * @file_to: file_to.
+ * error_file - checks if the file opened.
+ * @file_from: from file.
+ * @file_to: to file
  * @argv: arguments vector.
  * Return: no return.
  */
 void error_file(int file_from, int file_to, char *argv[])
 {
-	if (file_from == -1)
+	if (file_from < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
@@ -24,34 +24,34 @@ void error_file(int file_from, int file_to, char *argv[])
 
 /**
  * main - check the code for Holberton School students.
- * @argc: number of arguments.
- * @argv: arguments vector.
- * Return: Always 0.
+ * @argc: the number argument.
+ * @argv: the vector.
+ * Return: no return.
  */
-int main(int argc, char *argv[])
+int main(int ac, char *av[])
 {
 	int file_from, file_to, err_close;
-	ssize_t nchars, nwr;
+	ssize_t nchars, nu;
 	char buf[1024];
 
-	if (argc != 3)
+	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv);
+	file_from = open(av[1], O_RDONLY);
+	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, av);
 
 	nchars = 1024;
 	while (nchars == 1024)
 	{
 		nchars = read(file_from, buf, 1024);
 		if (nchars == -1)
-			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
+			error_file(-1, 0, av);
+		nu = write(file_to, buf, nchars);
+		if (nu == -1)
 			error_file(0, -1, argv);
 	}
 
