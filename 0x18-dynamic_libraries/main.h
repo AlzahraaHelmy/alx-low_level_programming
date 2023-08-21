c9241f2294d5bc1b3deaf4dafcdd2cc186254319
AlzahraaHelmy <zahra.helmy19113010@gmail.com>
2023-08-21 04:19:49 -0700

    b

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,25 +1,25 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char s);
-int _islower(int s);
-int _isalpha(int s);
-int _abs(int m);
-int _isupper(int s);
-int _isdigit(int s);
-int _strlen(char *t);
-void _puts(char *t);
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *t);
+int _atoi(char *s);
 char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int m);
-char *_strncpy(char *dest, char *src, int m);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-char *_memset(char *t, char b, unsigned int m);
-char *_memcpy(char *dest, char *src, unsigned int m);
-char *_strchr(char *t, char s);
-unsigned int _strspn(char *t, char *accept);
-char *_strpbrk(char *t, char *accept);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* _MAIN_H_ */
+#endif/*MAIN_H*/
