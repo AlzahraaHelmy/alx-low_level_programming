dd9b5f1ec8b1df04dee2bda10167640e4b456b13
AlzahraaHelmy <zahra.helmy19113010@gmail.com.com>
2023-09-07 10:55:03 -0700

    sda

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,19 +8,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *h_t;
+	hash_table_t *hash_table;
 	unsigned long int i;
 
-	h_t = malloc(sizeof(hash_table_t));
-	if (h_t == NULL)
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
 		return (NULL);
-
-	h_t->size = size;
-	h_t->array = malloc(sizeof(hash_node_t *) * size);
-	if (h_t->array == NULL)
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
 		return (NULL);
+	}
 	for (i = 0; i < size; i++)
-		h_t->array[i] = NULL;
-
-	return (h_t);
+		hash_table->array[i] = NULL;
+	return (hash_table);
 }
