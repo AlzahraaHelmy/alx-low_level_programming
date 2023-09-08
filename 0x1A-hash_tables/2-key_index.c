3e06f5928cb43682a6d058271a494b018782b096
AlzahraaHelmy <zahra.helmy19113010@gmail.com.com>
2023-09-08 11:32:06 -0700

    uhi

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * key_index - returns the index of a key
- * @key: key
- * @size: size of the array of the hash table
- * Return: index
+ * key_index - returns the index
+ * @key: key of index
+ * @size: size of the array
+ * Return: Index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
