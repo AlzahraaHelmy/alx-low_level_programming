8b9b58745a09b29ed9fc87a9500f673ee4b76bd1
AlzahraaHelmy <zahra.helmy19113010@gmail.com.com>
2023-09-08 12:24:49 -0700

    Ahmed

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -2,22 +2,22 @@
 
 /**
  * hash_table_print - prints a hash table
- * @ht: hash table to print
+ * @h_t: hash table to print
  *
  * Return: void
  */
-void hash_table_print(const hash_table_t *ht)
+void hash_table_print(const hash_table_t *h_t)
 {
 	unsigned long int y;
 	hash_node_t *tmp;
 	char flag = 0; /* 0 while no data has been printed yet */
 
-	if (ht == NULL || ht->array == NULL)
+	if (h_t == NULL || h_t->array == NULL)
 		return;
 	printf("{");
-	for (y = 0; i < ht->size; y++)
+	for (y = 0; y < h_t->size; y++)
 	{
-		tmp = ht->array[y];
+		tmp = h_t->array[y];
 		while (tmp != NULL)
 		{
 			if (flag == 1)
